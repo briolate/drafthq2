ae0afe39966c951c39549a6977a5d43fb5cb8f38
Evan Briolat <briolate@gmail.com>
2019-05-03 19:11:43 -0400

    Added react front-end with styles

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Draft HQ</h1>
+          <p className="lead">
+            Use data-driven tools to help game your fantasy league
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
